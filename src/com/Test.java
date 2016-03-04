da817a4a0e013d3c744413acf67705d9c5f7012d
huan045 <huan045@SHAW8NH1886.asycn.loc>
2016-03-04 15:57:21 +0800

    fffffff

---
+++ src/com/Test.java
@@ -0,0 +1,21 @@
+/*******************************************************************
+ * Copyright (c) 2007 arvato and others
+ * All rights reserved.
+ *
+ * Contributors:
+ * arvato Systems (Shanghai) Co., Ltd.
+ * 
+ ******************************************************************/
+package com;
+
+/**
+ * TODO (description of the Class)
+ *
+ * @author huan045
+ *
+ * @version 2016-3-4
+ *
+ */
+public class Test {
+
+}
